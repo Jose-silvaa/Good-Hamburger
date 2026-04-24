fd98baa7e84db070281ce26182487bd56c9f7768
Jose-silvaa <jvitorfacanha@gmail.com>
2026-04-24 00:19:17 -0300

    86aen6d3y

--- Features/Pedido/Interfaces/IPedidoService.cs
+++ Features/Pedido/Interfaces/IPedidoService.cs
@@ -1,4 +1,5 @@
 ﻿using GoodHamburger.Features.Pedido.Dtos;
+using GoodHamburger.Features.Produtos.Models;
 using GoodHamburger.Shared;
 
 namespace GoodHamburger.Features.Pedido.Interfaces;
@@ -9,7 +10,11 @@ public interface IPedidoService
 
      Task<IEnumerable<PedidoDto>> ObterTodosPedidos();
 
-     Task<Result<Models.Pedido>> ObterPedidoPorId(int id);
+     Task<Result<PedidoDto>> ObterPedidoPorId(int id);
      
      Task<Result<Models.Pedido>> ExcluirPedido(int id);
+     Task<Result<Models.Pedido>> AtualizarPedido(int pedidoId, AtualizarPedidoDto dto);
+     Task<Result<List<Produto>>> ObterEValidarProdutosAsync(IReadOnlyList<int> ids);
+
+     void AplicarProdutos(Models.Pedido pedido, List<Produto> produtos);
 }
--- Features/Pedido/Models/Pedido.cs
+++ Features/Pedido/Models/Pedido.cs
@@ -1,11 +1,16 @@
 ﻿using GoodHamburger.Features.Produtos.Dtos;
 using GoodHamburger.Features.Produtos.Models;
+using GoodHamburger.Shared;
 
 namespace GoodHamburger.Features.Pedido.Models;
 
 public class Pedido
 {
     public int Id { get; set; }
+    
+    public decimal Subtotal { get; private set; }
+    
+    public decimal Total { get; private set; }
     
     public int? SanduicheId { get; private set; }
     public Produto? Sanduiche { get; private set; }
@@ -19,22 +24,67 @@ public class Pedido
         {
             case TipoProduto.Sanduiche:
                 if (Sanduiche is not null)
-                    throw new InvalidOperationException("O Pedido já possui um sanduíche.");
+                    Result<Produto>.Fail("O pedido já possui um sanduíche.");
                 Sanduiche = produto;
                 SanduicheId = produto.Id;
                 break;
             
             case TipoProduto.Acompanhamento:
                 if (_acompanhamentos.Any(a => a.Id == produto.Id))
-                    throw new InvalidOperationException($"O acompanhamento '{produto.Nome}' já está no pedido.");
+                    Result<Produto>.Fail($"O acompanhamento '{produto.Nome}' já está no pedido.");
                 _acompanhamentos.Add(produto);
                 break;
         }
     }
 
-    public decimal Total()
+    public void CalcularSubTotal()
     {
-       return (Sanduiche?.Preco ?? 0) + _acompanhamentos.Sum(a => a.Preco);
+       Subtotal = (Sanduiche?.Preco ?? 0) + _acompanhamentos.Sum(a => a.Preco);
     }
-       
+
+    public void CalcularTotal()
+    {
+        decimal subtotal = 0m;
+
+        if (Sanduiche != null)
+            subtotal += Sanduiche.Preco;
+
+        subtotal += _acompanhamentos.Sum(a => a.Preco);
+
+        decimal desconto = CalcularDesconto(subtotal);
+        
+        Total = subtotal - desconto;
+    }
+
+    public decimal CalcularDesconto(decimal subtotal)
+    {
+        bool temSanduiche = Sanduiche != null;
+        bool temBatata = _acompanhamentos.Any(a => a.Nome == "Batata frita");
+        bool temRefrigerante = _acompanhamentos.Any(a => a.Nome == "Refrigerante");
+        
+        if (temSanduiche && temBatata && temRefrigerante)
+            return subtotal * 0.20m;
+
+        if (temSanduiche && temRefrigerante)
+            return subtotal * 0.15m;
+
+        if (temSanduiche && temBatata)
+            return subtotal * 0.10m;
+
+        return 0m;
+    }
+    
+    public void RemoverProdutos(IEnumerable<Produto> produtos)
+    {
+        var ids = produtos.Select(p => p.Id).ToHashSet();
+
+        if (SanduicheId.HasValue && ids.Contains(SanduicheId.Value))
+        {
+            SanduicheId = null;
+            Sanduiche = null;
+        }
+
+        _acompanhamentos.RemoveAll(p => ids.Contains(p.Id));
+    }
+
 }
